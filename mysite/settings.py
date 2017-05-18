0798cff0fc45c3d5a4ddf4947b915c4465d07bad
Heroinblackberry <buga-ga@yahdex.ru>
2017-05-19 01:43:53 +0400

    go

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e+btljk6t^js^*j=9hho!&m4*#by863-$-zzrc45265kh-$%y^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
